54c3c3117abb0de3c0b69a70e98c95a65dbb6977
João Vitor dos Santos Nunes <joaovitornunesdev@gmail.com>
2025-05-28 10:44:21 -0300

    finaldaauladia28

--- Teste0DS-main/exemplo0/Models/Pessoa.cs
+++ Teste0DS-main/exemplo0/Models/Pessoa.cs
@@ -2,6 +2,7 @@
 {
     public class Pessoa
     {
+        [Display(Name = "Código do Cliente")]
         public int PessoaId { get; set; }
         /*get é um método que retorna o valor atual de um atributo;
          set permite definir ou modificar o valor de um atributo
